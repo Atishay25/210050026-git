78e6ada2a7c4282d897ed111731c98a0c89f4575
Atishay25 <atishayjain2552@gmail.com>
2022-08-17 22:09:40 +0530

    Add polynomial rolling hash function

--- hashing.cpp
+++ hashing.cpp
@@ -1,10 +1,13 @@
 #include "hashing.hpp"
 
 int hash_string(string s) {
-    int n = s.length(), sum = 0;
+    int n = s.length(), var = 1;
     int m = 29;         // My Roll Number is 210050026
+    int p = 73;	
+    int sum = 0;
     for(int i = 0; i < n; i++){
-        sum += int(s[i] - '\0');
+        sum += (s[i])*var;
+	var = var*p;
     }
     return (sum % m);
 }
